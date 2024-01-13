7d652d19cbafd24f940bbd49cb53db8090040509
lazarowend <lazaroclans1@gmail.com>
2024-01-13 20:11:03 -0300

    add module specifications

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IResquest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    return false;
+  constructor(private specificationRepository: ISpecificationsRepository) {}
+  execute({ name, description }: IResquest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error('Specification already exists');
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    });
   }
 }
 
