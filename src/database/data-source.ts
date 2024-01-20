023e3f6455d7e611b8e9f2a6d4796fb9ca113e7d
lazarowend <lazaroclans1@gmail.com>
2024-01-20 06:01:37 -0300

    finish settings docker and postgres

---
+++ src/database/data-source.ts
@@ -0,0 +1,11 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: 'database',
+  port: 5432,
+  username: 'docker',
+  password: 'ignite',
+  database: 'rentx',
+  logging: true,
+});
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,27 @@
+import 'reflect-metadata';
+
+import { AppDataSource } from 'database/data-source';
 import express from 'express';
 import { router } from 'routes';
 import swaggerUi from 'swagger-ui-express';
 
 import swaggerFile from './swagger.json';
 
-const app = express();
+function startServer() {
+  const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(express.json());
-app.use(router);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+  app.use(express.json());
+  app.use(router);
 
-app.listen(8000, () => console.log('start server'));
+  app.listen(8000, () => console.log('start server'));
+}
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('database start!');
+    startServer();
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
